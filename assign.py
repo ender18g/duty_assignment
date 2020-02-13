8a1a1f149c151ad34d137057abf0afcc1fa4973e
Allan Elsberry <allan.elsberry@gmail.com>
2020-02-13 11:05:17 -0500

    added ability to increase max assignments

--- assign.py
+++ assign.py
@@ -6,6 +6,7 @@ from time import time
 import argparse
 from math import floor
 from tabulate import tabulate
+import copy
 
 parser = argparse.ArgumentParser(description='takes input.csv file and turns it into an optimized watchbill')
 parser.add_argument(
@@ -35,9 +36,9 @@ def make_watchbill():
     output_list = [v for v in best_dict.values()]
     output_list.sort(key=lambda x: x.get('date',0))
 
-    output = "Date\tName\tRank\tEmail\tDept\t\t\tBid\n"
+    output = "Date\tName\tRank\tEmail\tDept\t\tAssignments\tBid\n"
     for v in output_list:
-        output = output + f"\'{v['full_date']}\'\t\'{v['Duty Officer']}\'\t{v['Rank|Rate']}\t{v['Email']}\t{v['Dept']}\t\t\t{v['bid']}\n"
+        output = output + f"\'{v['full_date']}\'\t\'{v['Duty Officer']}\'\t{v['Rank|Rate']}\t{v['Email']}\t{v['Dept']}\t\t{v['assigned']}\t{v['bid']}\n"
     for line in best_leftover:
         output += "\t\t\t\t\t\t\t" + line.get('Duty Officer','-') + "\n"
     # print(output)
@@ -91,28 +92,43 @@ while True:
         break
     i+=1  #counts the number of iterations
     temp_score = 0
+    #starting a new try - clear out all of our temp bills
     temp_bill = {}
-    temp_personnel = personnel_bids[:]
+    temp_personnel = []
+    temp_personnel = copy.deepcopy(personnel_bids)
     #shuffle the list
     random.shuffle(date_list)
     random.shuffle(temp_personnel)
+    # print('##########################')
+    # for p in temp_personnel:
+    #  print(p['Duty Officer'] +"\t"+ str(p['assigned']))
     #print(temp_personnel[0]['Duty Officer'])
     for num,day in enumerate(date_list):
         #if you can't possibly make a BETTER score than already exists, STOP
         if (calendar_length-num)*2+temp_score<=max_score:
             break
-        #Sort personnel by the bid value for that day
+        #Sort personnel by the bid value for that day 2 to 0
         temp_personnel.sort(key=lambda x: x[day],reverse=True)
+        #sort personnel by number of assignments from 0 to 2
+        temp_personnel.sort(key=lambda x: x['assigned'],reverse=False)
+        #iterate through all personnel
         for n in range(len(temp_personnel)):
-            if temp_personnel[n].get('assigned',0)<max_assignments:
+            #if this person hasn't reached the max number of assignments
+            if temp_personnel[n]['assigned']<max_assignments:
+                #if this person did not give this day a 1 (do not assign)
                 if temp_personnel[n].get(day,1)!=1:
-                    temp_bill[day]=temp_personnel[n].copy()
+
                     bid_value = temp_personnel[n].get(day)
+                    #add their bid value to our overall points
                     temp_score=temp_score + bid_value
-                    #temp_personnel[n]['assigned']+=1
-                    temp_personnel.remove(temp_personnel[n])
+                    #mark this person as being assigned
+                    temp_personnel[n]['assigned']+=1
+                    #then assign this person to the day
+                    temp_bill[day]=temp_personnel[n].copy()
+                    #temp_personnel.remove(temp_personnel[n])
+                    #remember their bid for tracking purposes
                     temp_bill[day]['bid']=bid_value
-                    break
+                    break #move on to the next day
 
     if temp_score>max_score:
         if None in temp_bill or len(temp_bill)!=len(date_list):
@@ -121,6 +137,6 @@ while True:
             #print(f"new MAX!!  {temp_score}")
             max_score=temp_score
             best_dict=temp_bill.copy()
-            best_leftover = temp_personnel.copy()
+            best_leftover = [p for p in temp_personnel if p['assigned']==0]
             iter_num = i
             make_watchbill()
